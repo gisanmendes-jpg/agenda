e04c08b2a4993ab7663e99944c5e703f79bcba09
gisanmendes-jpg <gisanmendes@gmail.com>
2026-08-31 03:35:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Agenda Compartilhada", layout="wide")
 USUARIOS = {
     "gisa": "gisanmendes@gmail.com",
     "fabio": "fabioadriano044@gmail.com",
-    "andre": "gisanmendes@gmail.com"
+    "andre": "fgacursonline@gmail.com"
 }
 
 # Função disparadora de e-mails
